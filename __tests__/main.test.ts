d6c6736a07b476a8465a78f158e6438cbe756ac3
Jet Li <jing.i.qin@icloud.com>
2021-04-03 14:26:40 +0800

    Add logic

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,28 +1,15 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+import * as os from 'os'
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
+test('test runs with a fixed version v0.10.0', () => {
+  process.env['RUNNER_TEMP'] = os.tmpdir()
+  process.env['INPUT_VERSION'] = 'v0.10.0'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
+  const options: cp.ExecSyncOptions = {
     env: process.env
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  console.log(cp.execSync(`node ${ip}`, options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,80 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as io from '@actions/io'
+import * as tc from '@actions/tool-cache'
+import * as os from 'os'
+import * as path from 'path'
+import * as client from 'typed-rest-client/HttpClient'
+
+const c: client.HttpClient = new client.HttpClient('vsts-node-api')
+
+async function findVersionLatest(): Promise<string> {
+  core.info('Searching the latest version of trunk ...')
+  const response = await c.get(
+    'https://api.github.com/repos/thedodd/trunk/releases/latest'
+  )
+  const body = await response.readBody()
+  return Promise.resolve(JSON.parse(body).tag_name || 'v0.10.0')
+}
+
+async function findVersion(): Promise<string> {
+  const version: string = core.getInput('version')
+  if (version === 'latest' || version === null || version === undefined) {
+    return await findVersionLatest()
+  }
+  return Promise.resolve(version)
+}
 
 async function run(): Promise<void> {
+  const tempFolder = path.join(os.tmpdir(), 'setup-trunk')
+  await io.mkdirP(tempFolder)
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const version = await findVersion()
+    core.info(`Installing trunk ${version} ...`)
+    const platform = process.env['PLATFORM'] || process.platform
+    core.debug(platform)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    let ext = ''
+    let arch = ''
+    let arch_ext = '.tar.gz'
+    let extractFn = tc.extractTar
+    switch (platform) {
+      case 'win32':
+        ext = '.exe'
+        arch_ext = '.zip'
+        arch = 'x86_64-pc-windows-msvc'
+        extractFn = tc.extractZip
+        break
+      case 'darwin':
+        arch = 'x86_64-apple-darwin'
+        break
+      case 'linux':
+        arch = 'x86_64-unknown-linux-gnu'
+        break
+      default:
+        core.setFailed(`Unsupported platform: ${platform}`)
+        return
+    }
+    const archive = `trunk-${arch}`
+    const url = `https://github.com/thedodd/trunk/releases/download/${version}/${archive}${arch_ext}`
+    core.info(`Downloading trunk from ${url} ...`)
+    const downloadArchive = await tc.downloadTool(url)
+    core.info(`Extracting trunk to ${tempFolder} ...`)
+    const extractedBin = await extractFn(downloadArchive, tempFolder)
+    const execFolder = path.join(os.homedir(), '.cargo', 'bin')
+    await io.mkdirP(execFolder)
+    const exec = `trunk${ext}`
+    const execPath = path.join(execFolder, exec)
+    await io.mv(extractedBin, execPath)
+    core.info(`Installed trunk to ${execPath} 🎉`)
   } catch (error) {
     core.setFailed(error.message)
+  } finally {
+    io.rmRF(tempFolder)
   }
 }
 
-run()
+run().then(
+  () => core.info('Done'),
+  err => core.error(err)
+)
